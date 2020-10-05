14674c5ab438d0ffecb91b24c2bfd40dbb7a2b8c
Denis <denis.timoshitskiy@gmail.com>
2020-10-05 16:17:01 +0300

    Второе упражнение

--- script.js
+++ script.js
@@ -10,12 +10,29 @@ const personalMovieDB = {
 	privat: false
 };
 
-const answer1 = prompt('Один из последних просмотренных фильмов?', ''),
-	answer2 = prompt('На сколько оцените его?', ''),
-	answer3 = prompt('Один из последних просмотренных фильмов?', ''),
-	answer4 = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+	const answer1 = prompt('Один из последних просмотренных фильмов?', ''),
+				answer2 = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[answer1] = answer2;
-personalMovieDB.movies[answer3] = answer4;
+	if (answer1 != null && answer2 != null && answer1 != '' && answer2 != '' && answer1.length < 50) {
+		personalMovieDB.movies[answer1] = answer2;
+		console.log('Da');
+	} else {
+		console.log('error');
+		i--;
+	}
+
+}
+
+if (personalMovieDB.count < 10) {
+	console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+	console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+	console.log('Вы киноман');
+} else {
+	console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
+
